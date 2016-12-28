0759650c862df90c2c211647ad6f2dfe86bf118d
Abdullah Tellioğlu <abdullahtellioglu@tell10glu-MacBook-Air.local>
2016-12-29 01:35:36 +0200

    added fetcher

---
+++ src/com/wiki/pages/Fetcher.java
@@ -0,0 +1,8 @@
+package com.wiki.pages;
+
+public class Fetcher {
+	public static void fetch(Page page){
+		//JSOUP BURADA KULLANILACAK.
+		
+	}
+}
--- src/com/wiki/pages/Page.java
+++ src/com/wiki/pages/Page.java
@@ -1,11 +1,26 @@
 package com.wiki.pages;
 
 public class Page {
-	private String name;// sayfanin adi
-	private String content;//sayfadaki yazi
-	private Link[] links;//sayfada bulunan linkler
-	
+	protected String name;
+	protected String content;
+	protected Link[] links;
+	private String url;
 	public Page(String url){
-		
+		this.url = url;
 	}
+	public String getUrl(){
+		return url;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Link[] getLinks() {
+		return links;
+	}
+	
 }
